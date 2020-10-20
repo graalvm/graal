8dee3b3e7d62c790059da989b93acce314ac8bce
Yasser Bazzi <32601969+YShow@users.noreply.github.com>
2020-10-20 21:32:47 +0000

    Dont run the entire loop if the condition is true

--- compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/ProfileCompiledMethodsPhase.java
+++ compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/ProfileCompiledMethodsPhase.java
@@ -194,16 +194,15 @@ public class ProfileCompiledMethodsPhase extends Phase {
         return 2;
     }
 
-    private static boolean hasInvoke(Collection<Block> blocks) {
-        boolean hasInvoke = false;
+    private static boolean hasInvoke(Collection<Block> blocks) {        
         for (Block block : blocks) {
             for (FixedNode fixed : block.getNodes()) {
                 if (fixed instanceof Invoke) {
-                    hasInvoke = true;
+                   return true;
                 }
             }
         }
-        return hasInvoke;
+        return false;
     }
 
     @Override
