691182161ae6c58830dd08278e08d9a08e8b4381
Lazar Mitrović <lazar.mitrovic@msn.com>
2021-04-09 13:26:03 +0200

    Proper getStackAccessControlContext implementation
    Fixed imports and warnings in SecuritySubstitutions
    Implemented PrivilegedStack and use FastThreadLocal
    Added missing getProtectionDomain method
    Recompute contexts from static initializers in runtime
    Disallow NO_CONTEXT_SINGLETON in executePrivileged
    Work around crash when ProcessPropertiesSupport is missing getExecutable impementation

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/Target_jdk_internal_reflect_Reflection.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/Target_jdk_internal_reflect_Reflection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -36,7 +36,7 @@ public final class Target_jdk_internal_reflect_Reflection {
 
     @Substitute
     @NeverInline("Starting a stack walk in the caller frame")
-    private static Class<?> getCallerClass() {
+    static Class<?> getCallerClass() {
         return StackTraceUtils.getCallerClass(KnownIntrinsics.readCallerStackPointer(), true);
     }
 
